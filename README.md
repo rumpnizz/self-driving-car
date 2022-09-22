548923a93dea720dd051bcfb3d24d8d314627268
Markus Timour <markus.timour@vertiseit.se>
2022-09-22 22:17:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Self-driving car  - Machine learning
+
+---
+
+Big thanks to Radu for this amazing course!
+You can find his youtube channel [here](https://www.youtube.com/c/RaduMariescuIstodor).
+
+Simulates cars to drive on a highway.
+
+##### Run in browser.
+* Save button - Saves current neural brain to the browsers local storage.
+* Trash can - Deletes saved brain from neural storage.
+
+Don't ask me to explain the neural network. No idea :D
+
+
+After a couple of iterations, I got the brain saved in `src/js/data/best-car.js` \
+Uncomment the following in `index.html` to use this brain (local storage need to be empty):
+```
+<script src="js/car.js"></script>
+<!-- <script src="js/data/best-car.js"></script> --> <-- uncomment this line
+<script src="js/main.js"></script>
+```
---
+++ src/js/utils.js
@@ -0,0 +1,42 @@
+const lerp = (A, B, t) => A + (B - A) * t;
+
+const getIntersection = (A, B, C, D) => {
+    const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+
+    if (bottom == 0)
+        return null;
+
+    const tTop   = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+    const uTop   = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+
+    const t = tTop/bottom;
+    const u = uTop/bottom;
+
+    if (!(t >= 0 && t <= 1 && u >= 0 && u <= 1))
+        return null;
+
+
+    return {
+        x: lerp(A.x, B.x, t),
+        y: lerp(A.y, B.y, t),
+        offset: t
+    }
+}
+
+const polysIntersect = (poly1, poly2) => {
+    for (let i = 0; i < poly1.length; i++) {
+        for (let j = 0; j < poly2.length; j++) {
+            const touch = getIntersection(
+                poly1[i],
+                poly1[(i + 1) % poly1.length],
+                poly2[j],
+                poly2[(j + 1) % poly2.length]
+            )
+
+            if (touch)
+                return true;
+        }
+    }
+
+    return false;
+}
